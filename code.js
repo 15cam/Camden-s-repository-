1b233dd2968dcaacf517ca8962283224b43d0f58
15cam <ccopeland92274@uat.edu>
2025-10-23 13:38:21 -0700

    Add files via upload

--- code.js
+++ code.js
@@ -1,7 +1,9 @@
 function  updateText(){
       document.getElementById("heading").innerHTML = "javascript is Amazing!";
 }
+function playcraps(){
 
+}
 function PlayCraps(){
       console.log("PlayCraps() function was called");
 
@@ -11,7 +13,7 @@ let roll2 = rollDice();
 
 let sum = roll1 + roll2; 
 let result = "";
-if (sum> 5){
+if (sum> 5 && sum ==11){
   result = "You Lose! try again.";
       console.log("You Lose! try again.");
 }
@@ -36,3 +38,37 @@ function rollDice(){
       return Math.ceil(die);
 
 } 
+
+let intervalID=0;
+
+function startmove(){
+  let image = document.getElementById("memeimage");
+ intervalID = setInterval(function(){
+let xCord = getRandum();
+let yCord = getRandum();
+image.style.left = xCord + "px"
+image.style.left = yCord + "px"
+
+
+
+
+
+
+
+  },1000);
+
+   document.getElementById("btnStart").disabled=true;
+   document.getElementById("btnStop").disabled=false;
+
+
+  
+
+}
+function stopmove(){
+  clearInterval(intervalID);
+   document.getElementById("btnStart").disabled=false;
+   document.getElementById("btnStop").disabled=true;
+}
+function getRandum(){
+return Math.floor(Math.random() * 800);
+}
